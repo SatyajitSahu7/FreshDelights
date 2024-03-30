bc5b04427d0b968091a56e53aa1032caa5252214
Satyajit Sahu <satsah31@gmail.com>
2024-03-30 14:09:53 +0530

    minor changes

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,15 +3,7 @@ import {getFirestore} from 'firebase/firestore'
 import { getStorage} from 'firebase/storage'
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBcS7Ga-sZsDGKKD97jSjzjEcegYOx1AdY",
-//   authDomain: "foodzone01-47d24.firebaseapp.com",
-//   databaseURL: "https://foodzone01-47d24-default-rtdb.firebaseio.com",
-//   projectId: "foodzone01-47d24",
-//   storageBucket: "foodzone01-47d24.appspot.com",
-//   messagingSenderId: "589229505030",
-//   appId: "1:589229505030:web:c6c4d50417386e4ca8e187"
-// };
+
 const firebaseConfig = {
   apiKey: "AIzaSyBLrC5qHzs3qLpL7KCMJqt9fOwJiYbziDk",
   authDomain: "freshdelights-c0838.firebaseapp.com",
